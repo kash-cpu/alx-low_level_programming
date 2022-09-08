ece8f5926d450ba1f4f9f56247798bbe060b58fd
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-09-08 14:22:42 -0700

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - print the string in the put function
  *
@@ -8,6 +9,6 @@
  */ 
 int main(void)
 {
-	puts(*\"Programming is like building a multilingual puzzle*);
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return(0);
 }
