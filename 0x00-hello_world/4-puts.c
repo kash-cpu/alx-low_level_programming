b0a09995ee2d97f963ffd77e477d103365f9de5f
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-09-08 12:53:24 -0700

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
  * #include<stdio.h>: Defines input and output routines used by the program.
  * main: This is the entry oint of our program.
  * puts: the puts funcion prints out whats within the perentesis to standard output.
- * return: returns the integer to the caller.
+ * Return: 0
  */ 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
