cd4c354cf664d4252204e18b1ba9bd0b97da5bff
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-09-08 15:20:49 -0700

    solved 6-size.c task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("size of a char is: %ld byte(s)\n", sizeof(c));
+	printf("size of an int is: %ld byte(s)\n", sizeof(i));
+	printf("size of a long is: %ld byte(s)\n", sizeof(li));
+	printf("size of a long long int is: %ld byte(s)\n", sizeof(lli));
+	printf("size of a float is: %ld byte(s)\n", sizeof(f));
+}
