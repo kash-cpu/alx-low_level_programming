9063c32361ac8d67d2f3d6e106fd51a5b4263713
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-09-08 14:48:45 -0700

    solved 5-printf.c task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+/**
+ * main - print the string in the printf function
+ *
+ * Description: using the main function
+ * this program prints with proper grammer, but the outcome is a piece of art
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art\n");
+	return (0)
+}
