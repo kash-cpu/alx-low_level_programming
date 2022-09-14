2888a54e8267fdff549ccf73004b0143e475ae70
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-09-14 03:05:01 -0700

    correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * main - entrance point
- * Description: print_alphabet - function to print lower case alphabet
+ * print_alphabet - function to print lower case alphabet
+ *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
