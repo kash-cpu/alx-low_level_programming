b3ef077ef473037442f7f78f085d128463dfd442
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-09-14 04:20:28 -0700

    solved task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function to print lowercase alphabet 10 times
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char alphabet;
+	int count;
+
+	for (count = 10; count >= 0; count --)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+			_putchar(alphabet);
+			_putchar('\n');
+	}
+}
