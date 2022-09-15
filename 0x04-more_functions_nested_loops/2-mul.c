d4be17154bcb21c29658cfc31c2c347205f83786
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-09-15 08:25:30 -0700

    solved task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - gives the product of a and b
+ * @a: carrier variable
+ * @b: carrier variable
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
