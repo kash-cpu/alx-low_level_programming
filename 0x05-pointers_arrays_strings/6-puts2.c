2b19966b131bd57deb42153628b1851aeb1e7836
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-09-19 08:11:17 -0700

    correction

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - prints every other character
+ * @str: argument *str
+ * Return: string to stdout
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
