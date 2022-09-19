f786d660f0b067c2f974675ffd43bb8d40411880
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-09-19 07:48:30 -0700

    correction

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,16 +10,22 @@
 void rev_string(char *s)
 {
 	char c;
-	int l;
-	int i;
-	l = strlen(s);
+	int len1, len2, i;
 
-	for (i = 0; i<l/2; i++)
+	len1 = 0;
+	len2 = 0;
+
+	while (s[len1] != '\0')
+	{
+		len1++;
+	}
+
+	len2 = len1 - 1;
+	for (i = 0; i < len1 / 2; i++)
 	{
 		c = s[i];
-		s[i] = s[l - 1 - i];
-		s[l -1 - i] = c;
+		s [i] = s[len2];
+		s [len2] = c;
+		len2--;
 	}
-	_putchar(s);
-	_putchar('\n');
 }
