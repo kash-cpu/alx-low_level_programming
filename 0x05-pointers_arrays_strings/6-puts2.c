1bdc2b6554b5e968f9251cccebae79e632e7c253
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-09-19 08:16:14 -0700

    correction

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,10 +11,10 @@ void puts2(char *str)
 {
 	int i;
 
-	while (str[i] != '\0')
+	while (*str[i] != '\0')
 	{
 		if (i % 2 == 0)
-			_putchar(str[i]);
+			_putchar(*str[i]);
 		i++;
 	}
 	_putchar('\n');
