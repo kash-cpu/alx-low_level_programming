9130ebbbdd9171968872bd6fafe16821514b0a00
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-09-19 06:20:29 -0700

    correction

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,16 +10,16 @@ void print_rev(char *s)
 {
 	int i;
 
-	while (*s[i] != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
 
 	i--;
 
-	while (*s[i] != '\0')
+	while (s[i] != '\0')
 	{
-		_putchar(*s[i--]);
+		_putchar(s[i--]);
 	}
 	_putchar('\n');
 }
