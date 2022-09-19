73e280aea7ec126bff8aaddc173df4f82e1f572b
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-09-19 04:15:56 -0700

    solved task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: argument s
+ * Return: string lenght
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
