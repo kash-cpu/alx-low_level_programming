d0325ad12866c6d1692f7af9a1b8db486e697b49
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-09-19 06:57:08 -0700

    solved task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - function that reverse a string
+ * @s: argument s
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	char strrev(*s);
+	_putchar('\n');
+}
