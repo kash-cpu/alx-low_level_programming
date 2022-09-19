1f3058b48129cdac7324262a4a664345825e60da
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-09-19 02:05:54 -0700

    added header file and solved task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entrance point
+ * Description: reset_to_98 - a function that updates the reference of a pointer
+ * @n: pointer n to be updated
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
