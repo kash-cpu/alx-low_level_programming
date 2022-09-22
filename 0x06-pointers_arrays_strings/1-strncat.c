30c33d8c516f31843ca24357ba0603bceae0e398
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-09-22 01:20:53 -0700

    corrected task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -20,7 +20,7 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	for (j = 0; j < n && src[j] != '\0'; j++, i++)
 	{
-		dest[i] = srd[j];
+		dest[i] = src[j];
 	}
 	dest[i] = '\0';
 	return (dest);
