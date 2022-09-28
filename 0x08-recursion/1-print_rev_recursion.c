c12b4d7d7d4d9df1da270f1032a929677866fa1c
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-09-28 14:13:47 -0700

    correct betty

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * _print_rev_recursion - prints string in reverse
+ * @s: argument string
+ * Return: Always 0 (Success)
  */
 
 void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-	_print_rev_recursion (s + 1);
+	_print_rev_recursion(s + 1);
 	_putchar (*s);
 	}
 }
