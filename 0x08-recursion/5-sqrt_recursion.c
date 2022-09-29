9db7e04957b7f09d13f73223031ed9164dceec08
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-09-28 22:14:02 -0700

    correction

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,24 +1,4 @@
 #include "main.h"
-/**
- * myfunc - function to loop through given variable
- * @n: int argument
- * @result: int argument
- * Return: int square root
- */
-
-int myfunc(int n, int result)
-{
-	if (result * result == n)
-	{
-		return (result);
-	}
-	else if (result * result > n)
-	{
-		return (-1);
-	}
-	else
-		return (myfunc(n, result + 1));
-}
 
 /**
  * int _sqrt_recursion - natural square root of a number
@@ -32,5 +12,5 @@ int _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
-	return (helper(n, 0));
+	return (n, 0));
 }
