cae2634f1eb802bb7971e60137bb5d24fe8a7247
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-09-30 05:15:09 -0700

    solved task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - program that adds positive number
+ * @argc: number of command line argument and size of the argv array
+ * @argv: aray of argc
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	if (argc < 0)
+	{
+		printf("0\n");
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		sum = sum + atoi(argv[i]);
+
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!(isdigit(argv[i][j])))
+			{
+				printf("Error\n");
+				return (0);
+			}
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
