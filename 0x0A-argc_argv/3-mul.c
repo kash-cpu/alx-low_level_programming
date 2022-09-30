8b1f18d6967448daf92a028d99adbc5915047e86
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-09-30 04:46:29 -0700

    solved task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of command line argumens and size of argv array
+ * @argv: an array of argc
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
