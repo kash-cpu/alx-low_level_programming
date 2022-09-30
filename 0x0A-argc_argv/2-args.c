702fdea805e4b098fd4241abf0587e127b0fae9b
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-09-30 04:17:33 -0700

    solved task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all argument it receives
+ * @argc: number of command line arguments and size of the argv array
+ * @argv: an array of size argc
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
