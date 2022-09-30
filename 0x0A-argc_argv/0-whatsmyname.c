a1b1cfe8dc1f4565388b0af8a3fcb988c0be20e7
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-09-30 03:29:19 -0700

    added readme.md and solved task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print name of program
+ * @argc: size of argv array and number of command line arguments
+ * @argv: an array of argc
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv);
+	return (0);
+}
