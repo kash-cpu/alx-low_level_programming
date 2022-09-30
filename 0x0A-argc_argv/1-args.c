9f9efc9d7fc94d602529e4894a420b471e9dbebd
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-09-30 03:39:57 -0700

    solved task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of argument pass into it
+ * @argc: number of command line argument and size of the argv array
+ * @argv: an array of size argc
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%d\n", argc);
+}
