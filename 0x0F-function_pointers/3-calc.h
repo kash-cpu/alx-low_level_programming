08c8da49623efee7787a1239e543a644f40c45b2
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-10-12 08:01:08 -0700

    added README.md and solved task 0 to 3

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,18 @@
+#ifndef
+#define
+
+int (*get_op_func(char *s))(int, int);
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+    char *op;
+    int (*f)(int a, int b);
+} op_t;
+
+#endif
