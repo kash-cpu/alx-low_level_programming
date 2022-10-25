93cb3c62b9b5344ad9437fc5e26db700571167d5
kash-cpu <abdulazeez.kasheem@gmail.com>
2022-10-25 14:14:07 -0700

    solved task 6, 7, 8, 9

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Sum of all data (n)
+ * @head: Head of node
+ * Return: void
+ */
+
+int sum_listint(listint_t *head)
+{
+	unsigned int count = 0;
+
+	while (head != NULL)
+	{
+		count += head->n;
+		head = head->next;
+	}
+	return (count);
+}
